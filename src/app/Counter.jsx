271b605c4ad8274324f1391f00e426baa9cf90f8
utin-francis-peter <u.francispeter@gmail.com>
2023-06-01 15:28:14 +0100

    chore: subscribed component to counter store state

--- src/app/Counter.jsx
+++ src/app/Counter.jsx
@@ -1,12 +1,16 @@
+import { useSelector } from "react-redux";
 import Decrease from "../components/Decrease";
 import Increase from "../components/Increase";
 import Reset from "../components/Reset";
 
 const Counter = () => {
+  // subscribe component to needed store state
+  const counter = useSelector((state) => state.counter);
+
   return (
     <section className="counter-app">
       <div className="counter-app_inner">
-        <h2 className="display">Display</h2>
+        <h2 className="display">{counter.count}</h2>
         <div className="controls">
           <Increase />
           <Decrease />
